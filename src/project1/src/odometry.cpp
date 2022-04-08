1d987222ecec9860bcd2eacd1613b123af4d3568
manzomanze <manzella.davide97@gmail.com>
2022-04-08 15:24:48 +0200

    odometry node gets correct values from bag topics

--- src/project1/src/odometry.cpp
+++ src/project1/src/odometry.cpp
@@ -1,21 +1,19 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "sensor_msgs/JointState.h"
 #include "pub_sub/Num.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+void chatterCallback(const sensor_msgs::JointState::ConstPtr& msg) {
+  ROS_INFO("ODOMETRY position [%f],[%f],[%f],[%f]",msg->position[0],msg->position[1],msg->position[2],msg->position[3]);
+  ROS_INFO("ODOMETRY velocity [%f],[%f],[%f],[%f]\n",msg->velocity[0],msg->velocity[1],msg->velocity[2],msg->velocity[3]);
 }
 
-void counterCallback(const pub_sub::Num::ConstPtr& msg) {
-  ROS_INFO("I counted: [%d]", msg->num);
-}
+
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
 
-  ros::Subscriber sub_chatter = n.subscribe("chatter", 1000, chatterCallback);
-  ros::Subscriber sub_counter = n.subscribe("counter", 1000, counterCallback);
+  ros::Subscriber sub_encoder_wheel = n.subscribe("wheel_states", 1000, chatterCallback);
 
   ros::spin();
 
