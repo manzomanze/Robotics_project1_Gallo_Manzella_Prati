df0220ac7ecc2bc175aa2dd122b163adc059da3c
manzomanze <manzella.davide97@gmail.com>
2022-04-08 15:25:39 +0200

    Comment the pub node to understand it

--- src/pub_sub/src/pub.cpp
+++ src/pub_sub/src/pub.cpp
@@ -5,20 +5,22 @@
 #include <sstream>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "talker");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "talker"); //initialize the node
+  ros::NodeHandle n; //get a nodehandle to call the functions over this node 
 
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  ros::Publisher counter_pub = n.advertise<pub_sub::Num>("counter", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000); //advertise some type of message over a topic
+  ros::Publisher counter_pub = n.advertise<pub_sub::Num>("counter", 1000); //the Num message is described in the msg folder  of this package
 
   int count = 0;
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(10); // this limits the rate of the loop at 10 hz that means it publishes messages on the topics in 10 times per second 
 
-  while (ros::ok()) {
+  while (ros::ok()) { /*while (ros::ok()) is just a better way to write while(1): it’ll handle
+    interrupts and stop if a new node with the same name is created or a shutdown
+    command is called */
 
     // generate chatter msg
-    std_msgs::String msg;
+    std_msgs::String msg; 
     std::stringstream ss;
     ss << "hello world " << count;
     msg.data = ss.str();
@@ -34,9 +36,10 @@ int main(int argc, char **argv) {
     chatter_pub.publish(msg);
     counter_pub.publish(count_msg);
 
-    ros::spinOnce();
+    ros::spinOnce();/*ros::spin() will simply cycle as fast as possible, calling our callback
+    when needed, but without using CPU if there is nothing to do*/
 
-    loop_rate.sleep();
+    loop_rate.sleep(); // if we have finished before the looprate limited  at 10 Hz then we wait for the end of the time slot
 
     ++count;
   }
