eb81169d3863f2cc4ad0637f64f5729a24e26fd6
AndreaPrati98 <a.prati@outlook.it>
2022-04-18 11:21:54 +0200

    deleted prints for each msg, now it's everything each N msg

--- src/project1/src/odometry.cpp
+++ src/project1/src/odometry.cpp
@@ -39,30 +39,15 @@ public:
     t_msg actual_msg;
 
     actual_msg.seq = msg -> header.seq;
-    ROS_INFO("Message sequence: %d", actual_msg.seq);
-
+    
     actual_msg.time = msg -> header.stamp.toSec();
-    ROS_INFO("Time stamp: %f", actual_msg.time);
-
-
+    
     for (int i = 0; i < N_WHEELS; i++)
     {
       actual_msg.wheel_info.vel[i] = msg -> velocity[i];
       actual_msg.wheel_info.count_ticks[i] = msg -> position[i];
       
     }
-
-    ROS_INFO("Velocity (Rad/min): %g %g %g %g", actual_msg.wheel_info.vel[FL],
-                                                actual_msg.wheel_info.vel[FR],
-                                                actual_msg.wheel_info.vel[RL],
-                                                actual_msg.wheel_info.vel[RR]);
-    
-    ROS_INFO("Tick count: %g %g %g %g", actual_msg.wheel_info.count_ticks[FL],
-                                        actual_msg.wheel_info.count_ticks[FR],
-                                        actual_msg.wheel_info.count_ticks[RL],
-                                        actual_msg.wheel_info.count_ticks[RR]);
-
-    std::cout << std::endl;
     
     return actual_msg;
   }
@@ -80,6 +65,8 @@ public:
     ROS_INFO("ROBOT LINEAR VELOCITY ON X %f",robotLinearVelocityOnX);
     ROS_INFO("ROBOT LINEAR VELOCITY ON Y %f",robotLinearVelocityOnY);
     ROS_INFO("ROBOT ANGULAR VELOCITY %f",robotAngularVelocity);
+
+    std::cout << std::endl;
   }
 
   //callback called each time a message on topic /wheel_states is published by the bag
@@ -90,45 +77,19 @@ public:
     // if so, then we just save the actual message without computing anything
     // it's not a big deale because the first bunch of msgs are just in still position
     if(isFirstMeasure){
-      prevMsg = actual_msg;
       prevMsgEachNmsg = actual_msg;
       isFirstMeasure = 0;
       ROS_INFO("This was the first message, no delta are computed");
       return;
     }
 
-    // delta time computed at each msg
-    deltaTime = actual_msg.time - prevMsg.time;
-    ROS_INFO("Delta time: %f", deltaTime);
-
-    double computedVel[N_WHEELS];
-    // this for cycle is used to store and compute the velocity starting form the ticks
-    for (int i = 0; i < N_WHEELS; i++)
-    {
-      deltaPosition[i] = actual_msg.wheel_info.count_ticks[i] - prevMsg.wheel_info.count_ticks[i];
-      double tickPerSec = (deltaPosition[i]/deltaTime);
-      computedVel[i] = tickPerSec * RESOLUTION;
-    }
-
-    ROS_INFO("Delta ticks: %f %f %f %f", deltaPosition[FL], deltaPosition[FR],
-                                          deltaPosition[RL], deltaPosition[RR]);
-
-    ROS_INFO("Computed velocity: %f %f %f %f", computedVel[FL], computedVel[FR],
-                                                computedVel[RL], computedVel[RR]);
-
-    // print the velocity given in the bag, it is in Rad/min, so I divide by 60 to get the Rad/s
-    ROS_INFO("Velocity from the bag (Rad/sec): %f %f %f %f", actual_msg.wheel_info.vel[FL]/SEC_IN_MIN/4,
-                                                    actual_msg.wheel_info.vel[FR]/SEC_IN_MIN/4,
-                                                    actual_msg.wheel_info.vel[RL]/SEC_IN_MIN/4,
-                                                    actual_msg.wheel_info.vel[RR]/SEC_IN_MIN/4); 
-
-    // here the previous message is updated
-    prevMsg = actual_msg;
-
     // here I need to check that the four message are passed
     deltaMsgNumber = actual_msg.seq - prevMsgEachNmsg.seq;
-    // we enter this if each 4 messages
+    // we enter this if each N messages
     if(deltaMsgNumber == EVERY_N_MSG_TO_DENOISE){
+      
+
+      
       // now the delta time is performed between msgs that are distant 4 msgs
       deltaTime = actual_msg.time - prevMsgEachNmsg.time;
       double computedVelEachNmsg[N_WHEELS];
@@ -139,6 +100,18 @@ public:
         computedVelEachNmsg[i] = tickPerSec * RESOLUTION;
       }
 
+      ROS_INFO("Message sequence: %d", actual_msg.seq);
+      ROS_INFO("Time stamp: %f", actual_msg.time);
+      ROS_INFO("Velocity (Rad/min): %g %g %g %g", actual_msg.wheel_info.vel[FL],
+                                                actual_msg.wheel_info.vel[FR],
+                                                actual_msg.wheel_info.vel[RL],
+                                                actual_msg.wheel_info.vel[RR]);
+    
+      ROS_INFO("Tick count: %g %g %g %g", actual_msg.wheel_info.count_ticks[FL],
+                                        actual_msg.wheel_info.count_ticks[FR],
+                                        actual_msg.wheel_info.count_ticks[RL],
+                                        actual_msg.wheel_info.count_ticks[RR]);
+
       ROS_INFO("Delta ticks each four MSGS: %f %f %f %f", deltaPosEachNmsg[FL], deltaPosEachNmsg[FR],
                                                           deltaPosEachNmsg[RL], deltaPosEachNmsg[RR]);
 
@@ -146,14 +119,14 @@ public:
 
       ROS_INFO("Computed velocity each %d MSGS: %f %f %f %f", EVERY_N_MSG_TO_DENOISE, computedVelEachNmsg[FL], computedVelEachNmsg[FR],
                                                                 computedVelEachNmsg[RL], computedVelEachNmsg[RR]);
+      std::cout << std::endl;
+
+      
       // we reset the message 
       prevMsgEachNmsg = actual_msg;
 
       calculateKinematics(computedVelEachNmsg);
     }
-
-
-    std::cout << std::endl;
 
   }
 
@@ -169,16 +142,11 @@ public:
 private:
   int isFirstMeasure = 1;
   
-  t_msg prevMsg;
   // here we use a variable to store the message only after 4 messages
   // the idea is to limit the noise
-  t_msg prevMsgEachNmsg;
+ 
+  t_msg actual_msg, prevMsgEachNmsg;
 
-  t_msg actual_msg;
-
-
-
-  double deltaPosition[N_WHEELS];
   double deltaTime;
 
   // here we declare the variables used for the computation of the velocity with less noise (hopefully)
