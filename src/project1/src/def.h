f77ec24f68e5b106605e2a65bdd1b2224291ec1d
nicolas <nicolasg98@gmail.com>
2022-04-23 19:50:12 +0200

    the def file may be useless

--- src/project1/src/def.h
+++ src/project1/src/def.h
@@ -1,23 +1,42 @@
 #include "ros/ros.h"
 #include "param.h"
+#include "project1/parametersConfig.h"
+#include "geometry_msgs/PoseStamped.h"
 
 
-//odometry
-enum wheel_order {
-  FL,
-  FR,
-  RL,
-  RR
-};
 
-typedef struct {
-  double vel[N_WHEELS];
-  double count_ticks[N_WHEELS];
-} t_wheel_pos_vel;
 
-typedef struct {
-  uint32_t seq;
-  double time;
+// //odometry
+// struct t_wheel_pos_vel;
+// struct t_msg;
 
-  t_wheel_pos_vel wheel_info;
-} t_msg;
+// class OdometryCalculator{
+//   public:
+//     t_msg createStruct(const sensor_msgs::JointState::ConstPtr& msg);
+//     void encoderCallback(const sensor_msgs::JointState::ConstPtr& msg);
+//     void publishMsg_cmd_vel(double linear_x, double linear_y, double angular_z);
+//     void publishMsg_odom(double robot_x, double robot_y, double robot_theta);
+//     void calculateEulerIntegration(double linear_x, double linear_y, double angular_z, double samplingTime);
+//     void calculateRungeKuttaIntegration(double linear_x, double linear_y, double angular_z, double samplingTime);
+//     void calculateKinematics(double *computedVelEachNmsg );
+//     bool resetPose(project1::ResetPose::Request &req, project1::ResetPose::Response &res);
+    
+// };
+
+
+// //gt_pose
+// struct position_t;
+// struct orientation_t;
+// struct pose_t;
+
+// void printPosition(position_t position);
+// void printOrientation(orientation_t orient);
+// pose_t createStruct(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+// class PoseClass { 
+//   void poseCallBack(const geometry_msgs::PoseStamped::ConstPtr& msg);
+//   void param_callback(double* xPos, double* yPos, double* zPos,
+//                     double* xOrient, double* yOrient, double* zOrient, double* wOrient,
+//                     project1::parametersConfig &config, uint32_t level);
+
+// };
--- src/project1/src/gt_pose.cpp
+++ src/project1/src/gt_pose.cpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "project1/parametersConfig.h"
 #include <dynamic_reconfigure/server.h>
+#include "param.h"
 
 // these structs are used to encapsule in a cleaner way the message
 // which will be listened on the topic
