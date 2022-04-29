fab55240810ed2a1b6a9ae70ae9234fa5f1dd56b
manzomanze <manzella.davide97@gmail.com>
2022-04-29 13:52:25 +0200

    testing parameters to configure correct odometry

--- src/project1/src/param.h
+++ src/project1/src/param.h
@@ -3,11 +3,11 @@
 
 #define SEC_IN_MIN 60
 #define N_WHEELS 4
-#define N_WINDOWS 42
+#define N_WINDOWS 44//46 BAG3 MOLTO BENE forse 45 ancora meglio
 #define RESOLUTION (2*3.14/(N_WINDOWS))
 #define GEAR_RATIO 5
-#define X_WHEEL_DISTANCE 0.200
-#define Y_WHEEL_DISTANCE 0.169
-#define RADIUS_WHEEL 0.07
+#define X_WHEEL_DISTANCE 0.209//0.202 molto bene//0.200
+#define Y_WHEEL_DISTANCE 0.176//0.170 molto bene//0.169
+#define RADIUS_WHEEL 0.08
 #define EVERY_N_MSG_TO_DENOISE 3
 #define DEBUG 0
