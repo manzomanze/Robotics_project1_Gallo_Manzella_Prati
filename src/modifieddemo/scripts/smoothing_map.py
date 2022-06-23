a87358a4b5abf463c3e4c573ba6669703c98e896
manzomanze <manzella.davide97@gmail.com>
2022-06-23 18:29:57 +0200

    saves using the service

--- src/modifieddemo/scripts/smoothing_map.py
+++ src/modifieddemo/scripts/smoothing_map.py
@@ -3,17 +3,20 @@ import rospy
 import numpy as np
 import cv2
 from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Path
 import os
+from modifieddemo.srv import SaveMap
+
 
 
 cwd = os.getcwd()
-pub = rospy.Publisher('my_map', OccupancyGrid, queue_size=1, latch=True)
 
 
-def callback(data):
+def callbackMap(data):
     width = data.info.width
     height = data.info.height
     size =  (width , height)
+    global image
     image = np.zeros(size)
     
     counter = 0
@@ -32,7 +35,6 @@ def callback(data):
     custom_map.header = data.header
     custom_map.info = data.info
     custom_map.data =  np.transpose(image).flatten().astype (int)
-    pub.publish(custom_map)
 
 
 
@@ -40,28 +42,26 @@ def callback(data):
     image[image == -1] = 150
     image[image == 0] = 255
     image[image == 100] = 0
-    
+    print("AAAAAAAAAAAAAAAAAAAAAAAa")
+
+def callbackPath(data):
+    poses = data.poses
+
+def saver(req):
     filename = cwd+'/grid.png'
-    print ("saving image")
+    print (cwd+'/grid.png'+"saving image")
     cv2.imwrite (filename,image)
-
-
-    
-
- 
-  
+    return 200
 
 def listener():
-
+    print ("starting")
     rospy.init_node('depth_saver', anonymous=False)
 
-    rospy.Subscriber("/map", OccupancyGrid, callback)
-    
+    rospy.Subscriber("/map", OccupancyGrid, callbackMap)
+    rospy.Subscriber("/trajectory", Path, callbackPath)
 
+    savemap = rospy.Service('saveMap', SaveMap, saver)
     
-    
-    
-   
     rospy.spin()
 
 if __name__ == '__main__':
